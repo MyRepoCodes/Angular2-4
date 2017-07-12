df7131f51440c061da6d09fd9e89fe7dd2c0a3bd
My public Repo <libraravipandey677@gmail.com>
2017-07-13 02:10:45 +0530

    Update environment.orange.ts

--- src/environments/environment.orange.ts
+++ src/environments/environment.orange.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   name: 'orange',
-  baseUrl: 'https://api.synergysportstech.com/preproduction',
-  loginUrl: 'http://10.3.0.207/Core/login?site=serverweb&url=',
-  encodingUrl: 'https://api.synergysportstech.com/EncodingApi/api',
-  ticketUrl: 'http://ops-issue-tracker-staging.azurewebsites.net',
+  baseUrl: '*****************************',
+  loginUrl: '*****************************',
+  encodingUrl: '*****************************',
+  ticketUrl: '*****************************',
   topBarColor : '#ffa500',
   version: '2.0.14'
 };
