222f0ca9defa1a4483bdfa197eb75ea13cdcfc42
My public Repo <libraravipandey677@gmail.com>
2017-07-13 02:08:38 +0530

    Update environment.yellow.ts

--- src/environments/environment.yellow.ts
+++ src/environments/environment.yellow.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   name: 'yellow',
-  baseUrl: 'http://yellow.data.synergysportstech.com',
-  loginUrl: 'http://10.3.0.207/Core/login?site=serverweb&url=',
-  encodingUrl: 'http://yellow.data.synergysportstech.com/EncodingApi/api',
-  ticketUrl: 'http://test-ticketing.synergysportstech.com',
+  baseUrl: '********************',
+  loginUrl: '**************************',
+  encodingUrl: '*********************************',
+  ticketUrl: '**************************************',
   topBarColor : '#d8d815',
   version: '2.0.14'
 };
